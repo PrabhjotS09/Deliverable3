00db26012f8c5c4e90de741064d3ef68d6b7d4ad
PrabhjotS09 <97964173+PrabhjotS09@users.noreply.github.com>
2022-04-23 15:20:23 -0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,44 @@
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+package cardproject;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author dancye
+ * @author Tejus Singh Gill
+ * date 20 Nov,2020
+ */
+public  class Card {
+     
+    //default modifier for child classes
+    private final Face faces;
+    private final Suit suits;
+
+    public Card(Face faces, Suit suits) {
+        this.faces = faces;
+        this.suits = suits;
+    }
+
+    public Face getFaces() {
+        return faces;
+    }
+
+    public Suit getSuits() {
+        return suits;
+    }
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public String toString(){
+          return faces + " of " + suits;
+    }
+
+}
